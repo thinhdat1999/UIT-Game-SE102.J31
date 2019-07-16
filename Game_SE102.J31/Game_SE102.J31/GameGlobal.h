ba57470b87eaf22e501bc75c3d96770167bc48af
thinhdat1999 <32608183+thinhdat1999@users.noreply.github.com>
2019-07-16 11:50:48 +0700

    Add files via upload
    Upload file

---
+++ Game_SE102.J31/Game_SE102.J31/GameGlobal.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <Windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <dinput.h>
+#include <WinUser.h>
+#include <unordered_map>
+
+#define SCREEN_WIDTH 320			// Chiều rộng cửa sổ
+#define SCREEN_HEIGHT 176			// Chiều dài cửa sổ
+#define MAX_FRAME_RATE 120				// FPS
+#define WIN_NAME "Captain America and The Avengers"
+#define WIN_TITLE "Captain America and The Avengers"
+#define KEYBOARD_BUFFER_SIZE 1024
+
+
+extern LPD3DXSPRITE spriteHandler; //con tro sprite
+extern LPDIRECT3DDEVICE9 d3ddev; //con tro den thiet bi
+extern LPDIRECT3DSURFACE9 backBuffer; //
+extern LPDIRECT3DSURFACE9 surface;
+extern bool isGameRunning;
+extern std::unordered_map<int, bool> keyCode;
+
+
+struct Rect {
+	float x, y;
+	float width, height;
+	Rect() {}
+	Rect(float x, float y, float width, float height)
+	{
+		this->x = x;
+		this->y = y;
+		this->width = width;
+		this->height = height;
+	}
+	bool isContain(Rect r)
+	{
+		return !((x + width < r.x) || (x > r.x + r.width) || (y < r.y - r.height) || (y - height > r.y));
+	}
+};
---
+++ Game_SE102.J31/Game_SE102.J31/WinMain.cpp
@@ -0,0 +1,15 @@
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "Game.h"
+#include "PlayScene.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int cmdShow)
+{
+	//Game *game = Game::GetInstance();
+
+	//game->GameInit(hInstance, cmdShow);
+	//game->GameStartUp();
+	//game->GameRun();
+	return 0;
+}
